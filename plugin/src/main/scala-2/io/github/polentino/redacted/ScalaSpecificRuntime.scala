7e3abb7ed3ef8a137245819458b27989ebc3de1b
Diego Casella <polentino911@gmail.com>
2025-03-02 22:01:50 +0100

    feat: abstract validation code (wip)

--- plugin/src/main/scala-2/io/github/polentino/redacted/ScalaSpecificRuntime.scala
+++ plugin/src/main/scala-2/io/github/polentino/redacted/ScalaSpecificRuntime.scala
@@ -5,14 +5,39 @@ import scala.tools.nsc.Global
 // this won't work, because we need a hold of `global` type ahead of creation
 // final case class ScalaSpecificRuntime[GlobalRef <: Global](global: Global) extends RuntimeApi { .. }
 trait ScalaSpecificRuntime[GlobalRef <: Global] extends RuntimeApi {
-  val theGlobal: GlobalRef
+  protected val theGlobal: GlobalRef
   override type Tree = theGlobal.Tree
+  override type MethodDef = theGlobal.DefDef
+  override type Symbol = theGlobal.Symbol
+  override type Position = theGlobal.Position
+
+  // todo: move to RuntimeApi
+  private val REDACTED_CLASS: String = "io.github.polentino.redacted.redacted"
+  private lazy val toStringTermName = theGlobal.TermName("toString")
+
+  protected def extractMethodDefinition(tree: Tree): Option[MethodDef] = tree match {
+    case d: MethodDef if d.symbol.owner.isCaseClass => Some(d)
+    case _                                       => None
+  }
+
+  protected def isToString(defDef: MethodDef): Option[MethodDef] =
+    if (defDef.name == toStringTermName) Some(defDef) else None
+
+  protected def getRedactedFields(tree: Tree): Option[List[Symbol]] =
+    tree.symbol.owner
+      .primaryConstructor
+      .paramss
+      .flatten
+      .filter(_.annotations.exists(_.symbol.fullName == REDACTED_CLASS)) match {
+      case Nil            => None
+      case redactedFields => Some(redactedFields)
+    }
 }
 
 object ScalaSpecificRuntime {
 
   def apply(global: Global): ScalaSpecificRuntime[global.type] = new ScalaSpecificRuntime[global.type ] {
-    override val theGlobal: global.type  = global
+    override protected val theGlobal: global.type  = global
     import global._
   }
 }
--- plugin/src/main/scala/io/github/polentino/redacted/RuntimeApi.scala
+++ plugin/src/main/scala/io/github/polentino/redacted/RuntimeApi.scala
@@ -2,7 +2,26 @@ package io.github.polentino.redacted
 
 trait RuntimeApi {
   type Tree
+  type MethodDef
+  type Symbol
   type Position
 
-  def process(tree: Tree): Tree = tree
+  final def process(tree: Tree): Tree = validate(tree) match {
+    case Some(_) =>
+      println("TREE IS VALID")
+      tree
+    case None =>
+      println("TREE IS NOT VALID")
+      tree
+  }
+
+  private final def validate(tree: Tree): Option[MethodDef] = for {
+    defDefInCaseClass <- extractMethodDefinition(tree)
+    toStringDefDef <- isToString(defDefInCaseClass)
+    _ <- getRedactedFields(tree)
+  } yield toStringDefDef
+
+  protected def extractMethodDefinition(tree: Tree): Option[MethodDef]
+  protected def isToString(defDef: MethodDef): Option[MethodDef]
+  protected def getRedactedFields(symbol: Tree): Option[List[Symbol]]
 }
