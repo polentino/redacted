cd9c9b7c09bfc0ecda874391c94f869af3f481e5
Diego Casella <polentino911@gmail.com>
2025-04-23 12:56:51 +0200

    docs: update broken link

--- redacted-docs/src/pages/index.tsx
+++ redacted-docs/src/pages/index.tsx
@@ -18,7 +18,7 @@ function HomepageHeader() {
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/docs/intro">
+          <Link className="button button--secondary button--lg" to="/docs/category/installation">
             Curious? Let's get started 🛠️
           </Link>
         </div>
